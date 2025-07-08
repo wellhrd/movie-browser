94a8732f6971fe58954a62998614f9f46160b886
wellhrd <lil_rock15@hotmail.com>
2025-07-08 16:20:08 -0400

    Updated dependencies and start of stack navigator

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+// Create a stack navigator
+const Stack = createStackNavigator();
+
+// Define Home screen
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button title='Go to details' onPress={() => navigation.navigate('Details')} />
+    </View>
+  );
+}
+
+// Define Details screen
+function DetailsScreen() {
+  return (
+    <View style={styles.container}>
+      <Button title='Go back' onPress={() => navigation.navigate('Home')} />
+    </View>
+  );
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      // <View style={styles.container}>
+      //   <Text>Open up App.js to start working on your app!</Text>
+      // </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen}  />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
@@ -19,3 +49,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
